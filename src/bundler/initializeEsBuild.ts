4fcbd79c9c44b442826673b7ad1d6260289f1981
Artem <golysheeet@gmail.com>
2023-07-11 19:40:24 +0300

    fixed bug with esbuild initializer

--- src/bundler/initializeEsBuild.ts
+++ src/bundler/initializeEsBuild.ts
@@ -1,7 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 
-export default async function initialize() {
-  await esbuild.initialize({
+export default function initializeEsBuild() {
+  return esbuild.initialize({
     worker: true,
     wasmURL: 'https://unpkg.com/esbuild-wasm@0.17.19/esbuild.wasm',
   });
